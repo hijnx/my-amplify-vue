40050b2687cd22cc7b829572d892b05035ce89ee
Jin Park <ima.jin.park@gmail.com>
2021-04-14 15:33:38 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <amplify-authenticator>
+    <!-- The rest of your app code -->
+    <div id="app">
+      <h1>Todo App</h1>
+      <input type="text" v-model="name" placeholder="Todo name">
+      <input type="text" v-model="description" placeholder="Todo description">
+      <button v-on:click="createTodo">Create Todo</button>
+      <div v-for="item in todos" :key="item.id">
+        <h3>{{ item.name }}</h3>
+        <p>{{ item.description }}</p>
+      </div>
+    </div>
+    <amplify-sign-out></amplify-sign-out>
+  </amplify-authenticator>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { API } from 'aws-amplify';
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  async created() {
+    this.getTodos();
+  },
+  data() {
+    return {
+      name: '',
+      description: '',
+      todos: []
+    }
+  },
+  methods: {
+    async createTodo() {
+      const { name, description } = this;
+      if (!name || !description) return;
+      const todo = { name, description };
+      this.todos = [...this.todos, todo];
+      await API.graphql({
+        query: createTodo,
+        variables: {input: todo},
+      });
+      this.name = '';
+      this.description = '';
+    },
+    async getTodos() {
+      const todos = await API.graphql({
+        query: listTodos
+      });
+      this.todos = todos.data.listTodos.items;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<!--
+<script>
+// Real-time data with GraphQL subscriptions
+// Now if you wish to subscribe to data, import the onCreateTodo subscription and create a new subscription by adding subscription with API.graphql() like so:
+
+// other imports
+import { onCreateTodo } from './graphql/subscriptions';
+
+export default {
+  // other functions and properties
+  created(){
+    this.getTodos();
+    this.subscribe();
+  },
+  methods: {
+    // other methods
+    subscribe() {
+      API.graphql({ query: onCreateTodo })
+        .subscribe({
+          next: (eventData) => {
+            let todo = eventData.value.data.onCreateTodo;
+            if (this.todos.some(item => item.name === todo.name)) return; // remove duplications
+            this.todos = [...this.todos, todo];
+          }
+        });
+    }
+  }
+};
+</script>
+-->
